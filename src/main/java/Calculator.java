fe02bc836c98f327a0de801831ae8862f64b1c81
sujeetray96 <sujeetray96@gmail.com>
2023-05-21 14:02:49 +0530

    test custom delimiter

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,7 +3,17 @@ public class Calculator {
         if (numbers.isEmpty()) {
             return 0;
         }
-        String[] arr = numbers.split("[,\\n]");
+
+
+        String delimiter = ",";
+        String secondNumbers = numbers;
+
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterIndex);
+            secondNumbers = numbers.substring(delimiterIndex + 1);
+        }
+        String[] arr = secondNumbers.split("[,\n"+delimiter+"]");
         int sum = 0;
         for (String num : arr) {
             sum += Integer.parseInt(num);
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -37,6 +37,11 @@ public class CalculatorTest {
         int result = calculator.add("1\n2,3");
         Assertions.assertEquals(6, result);
     }
-
+    @Test
+    public void testCustomDelimiter() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add("//;\n1;2");
+        Assertions.assertEquals(3, result);
+    }
 
 }
